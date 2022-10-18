604376897ddb65389674a453e126bb3cc95b5bc9
william-mukere <gutsmukere@gmail.com>
2022-10-18 11:36:05 -0700

    using puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 	 */
 	int main(void)
 	{
-	char Mystr[] ="\"Programming is like building a multilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
