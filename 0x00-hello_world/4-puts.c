f42f7bdb782135c28513fc021cf4224b1162cbc2
william-mukere <gutsmukere@gmail.com>
2022-10-18 11:18:09 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+	#include<stdio.h>
+
+	/**
+	 *main -Entry point
+	 *Return: Always 0 (Success)
+	 *
+	 * description:the cde will print out the string
+	 */
+	int main(void)
+	{
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+	}
