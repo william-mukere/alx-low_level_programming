c54638deb6fd0acdb05a4870119fb6b35f8ebbd0
william-mukere <gutsmukere@gmail.com>
2022-10-18 13:12:06 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 	#include<stdio.h>
 
 	/**
-	 *main -Entry point
-	 *Return: Always 0 (Success)
+	 * main -Entry point
 	 *
-	 * description:the cde will print out the string
+	 * Return : 0
 	 */
 	int main(void)
 	{
