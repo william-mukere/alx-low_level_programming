be7d1c8fdd07b642b07a2c194f96ab502858d906
william-mukere <gutsmukere@gmail.com>
2022-10-24 03:22:34 -0700

    print statement using other command apart form put and printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-	#include <stdio.h>
-	/**
-	 * main -Entry point
-	 * return : 0
-	 */
-	int main(void)
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry point of the program.
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
-}}
+}
