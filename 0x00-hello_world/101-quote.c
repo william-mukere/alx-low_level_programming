8e7dabd48a2f868710d8895ac48d634746b91e47
william-mukere <gutsmukere@gmail.com>
2022-10-24 03:30:40 -0700

    use write function to display words

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
 * main - Entry point of the program.
-* Return: Always 0 (Success)
+* Return: Always 0I (Success)
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
