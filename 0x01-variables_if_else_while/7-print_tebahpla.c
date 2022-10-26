e07d48d7c508ad057448dfe36bca61bad4c19237
william-mukere <gutsmukere@gmail.com>
2022-10-26 14:46:06 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 'z';
+	{
+	for (n = 'z' ; n >= 'a' ; n--)
+
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
