0eced35e6667a6ac684f3bd372e6d605c23e7fe0
william-mukere <gutsmukere@gmail.com>
2022-10-26 14:39:49 -0700

    print 0 to 9 using putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+/**
+ * main -Entry point of the program.
+ * Return: Always  0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
