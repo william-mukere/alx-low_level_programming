c03487cfd8c36bdda23ff351d47d77c9cb667ba8
william-mukere <gutsmukere@gmail.com>
2022-10-26 14:18:59 -0700

    print 0 to 9 using printf command

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 'a';
+	{
+	for (n = 'a' ; n != 'q' ; n <= 'z' ; n++)
+
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
