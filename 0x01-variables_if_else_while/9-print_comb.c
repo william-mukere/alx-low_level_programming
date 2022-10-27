24d3e1449a4e31c46cfe375031864a344c270a56
william-mukere <gutsmukere@gmail.com>
2022-10-26 23:57:14 -0700

    print single characters separated by comma using putchar()

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include  <stdio.h>
+/**
+ * main -Entry point of the program.
+ * Return: Always  0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+		if  (n == '9') continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
