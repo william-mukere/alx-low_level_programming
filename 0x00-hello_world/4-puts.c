6f69e548ff7b31e50cb69409cc2772c77a201661
william-mukere <gutsmukere@gmail.com>
2022-10-27 23:05:12 -0700

    use put() to print statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
-	#include<stdio.h>
+#include<stdio.h>
 
-	/**
-	 * main -Entry point
-	 *
-	 * Return : 0
-	 */
-	int main(void)
-	{
+/**
+ * main -Entry point
+ * Return: 0
+ */
+int main(void)
+{
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
 	return (0);
-	}
+}
