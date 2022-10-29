f8e2e6397205faf7603834ed54f719be9f691e64
william-mukere <gutsmukere@gmail.com>
2022-10-29 13:08:55 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+/**
+ * _putchar - The purpose of this function is to print putchar
+ * Return:Always 0 (Success)
+ */
+int _putchar(char _putchar)
+{
+	return (write(1, &_putchar, 1));
+	return (0);
+}
