e703013d309f7a3f3655a29167aef519489c9d47
william-mukere <gutsmukere@gmail.com>
2022-11-09 03:16:56 -0800

    print alphabet in lowercase 10 times followed by newline

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int n = 'a', i = 0;
+	{
+		for (i = 0 ; i <= 9 ; i++)
+		{
+			for (n = 'a' ; n <= 'z' ; n++)
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
