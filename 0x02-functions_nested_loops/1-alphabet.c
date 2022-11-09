e18ecac2bb55c7162a7618be48a4ee6f633d5112
william-mukere <gutsmukere@gmail.com>
2022-11-09 02:19:53 -0800

    print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+	{
+		for (n = 'a' ; n <= 'z' ; n++);
+		_putchar(n);
+	}
+	_putchar('\n');
+	return ;
+}
