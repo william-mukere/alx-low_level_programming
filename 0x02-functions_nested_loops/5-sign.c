10e20376fbbaa7abc78fc8fc8a732202ee143ac7
william-mukere <gutsmukere@gmail.com>
2022-11-14 08:35:44 -0800

    print sign of a character whether -ve /+ve or xero

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "main.h"
 /**
  * print_sign - Check if the character is + or -
  * @n: The character
@@ -8,12 +9,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
-	return (1);
+		return (1);
+	}
 	else if (n == 0)
+	{
 		_putchar(0);
-	return (0);
+		return (0);
+	}
 	else
+	{
 		_putchar('-');
-	return ('-1');
+		return ('-1');
+	}
 }
