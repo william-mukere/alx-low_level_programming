c9c108485d2fa5e4bd3d18c5c927e8b2cf0f7d70
william-mukere <gutsmukere@gmail.com>
2022-11-14 08:26:41 -0800

    print whether character is positive or negative or zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+ * print_sign - Check if the character is + or -
+ * @n: The character
+ * Return: 1 if character is +ve , 0 if character
+ * is zero and -1 if character is -ve.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+	return (1);
+	else if (n == 0)
+		_putchar(0);
+	return (0);
+	else
+		_putchar('-');
+	return ('-1');
+}
