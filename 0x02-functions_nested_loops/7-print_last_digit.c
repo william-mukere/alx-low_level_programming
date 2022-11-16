ca2c4138d3124894518ebc2f175c8f9b5ec0406d
william-mukere <gutsmukere@gmail.com>
2022-11-16 06:56:32 -0800

    print last digit of a value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * print_last_digit - print last digit of a value
+ * @n: character 
+ * Return: Always return n %10
+ */
+int print_last_digit(int n)
+{
+	return ( n % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,7 +13,7 @@ int print_sign(int n);
 
 int _abs(int n);
 
-int print_last_digit(int);
+int print_last_digit(int n);
 
 void jack_bauer(void);
 
