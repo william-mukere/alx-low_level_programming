857da383c4e0507dc4d4ace4210109efa65cb3ee
william-mukere <gutsmukere@gmail.com>
2022-11-16 03:54:13 -0800

    return absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,15 +9,17 @@ int _abs(int n)
 {
 	if (n < 0)
 	{
-	_putchar(n);
+		_putchar(n);
+		return (+);
 	}
 	else if (n > 0)
 	{
 		_putchar(n);
+		return (+);
 	}
 	else
 	{
 		_putchar(0);
+		return (0);
 	}
-	return (0);
 }
