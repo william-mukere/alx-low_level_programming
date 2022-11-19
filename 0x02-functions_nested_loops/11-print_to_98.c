2c85d1eb16fcfb7976245d66badcdc17fb5f45be
william-mukere <gutsmukere@gmail.com>
2022-11-19 06:34:31 -0800

    print numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (c = n; c <= 98; c++)
+		for (c ='n'; c <= '98'; c++)
 		{
 			_putchar(c);
 			_putchar(',');
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (c = n; c >= 98; c--)
+		for (c = 'n'; c >= '98'; c--)
 		{
 			_putchar(c);
 			_putchar(',');
