0acc899106690b4f2e8a6198faf2095cd23416ee
william-mukere <gutsmukere@gmail.com>
2022-12-20 02:00:12 -0800

    check whether a letter is uppercase or not

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check whether a letter is uppercase
+ * @c: character to pass
+ * Return: 1 if uppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
