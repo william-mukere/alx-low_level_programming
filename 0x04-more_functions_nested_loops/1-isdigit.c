38e45dfafdd38270f7e28c44c334ae5ba4c47c88
william-mukere <gutsmukere@gmail.com>
2023-01-25 10:11:21 -0800

    write a function that check digit from 0 through 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigit - check whether a digit is between 0 and 9
  * @c: integer
- * Return: 1 if digit 0 otherwiset number
+ * Return: 1 if digit 0 otherwise
  */
 int _isdigit(int c)
 {
