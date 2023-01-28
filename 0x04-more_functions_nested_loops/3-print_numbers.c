016cce5895a057f2d047a52c6475f28ea36a580a
william-mukere <gutsmukere@gmail.com>
2023-01-28 09:55:50 -0800

    print number 0 through 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 through 9
+ * Return:Always return nothing
+ */
+void print_numbers(void)
+{
+	int c;
+
+	if (c >= 0, c <= 9, i++)
+	{
+		_putchar(c);
+	}
+	_putchar(\n);
+	return ();
+}
