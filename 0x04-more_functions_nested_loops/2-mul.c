9ec9396a757066522ea018b6824f240f87ac4b26
william-mukere <gutsmukere@gmail.com>
2023-01-28 08:18:30 -0800

    function that give product of two numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,9 @@
  * mul - multiple two numbers
  * @a:first Integer
  * @b:second Integer
- * @c: product of a by b
  * Return: Always return c
  */
-int mul(int a,int b)
+int mul(int a, int b)
 {
-	int c;
-	c = a * b;
-	return (c);
+	return (a * b);
 }
