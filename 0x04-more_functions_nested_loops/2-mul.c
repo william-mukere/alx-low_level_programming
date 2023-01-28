982394bd356aaec86bb6e9434192a0d23b74f337
william-mukere <gutsmukere@gmail.com>
2023-01-28 08:02:50 -0800

    write a function that give a product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiple two numbers
+ * @a:first Integer
+ * @b:second Integer
+ * @c: product of a by b
+ * Return: Always return c
+ */
+int mul(int a,int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
